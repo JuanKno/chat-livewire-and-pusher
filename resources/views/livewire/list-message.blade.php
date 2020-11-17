2d4b885b68a6e2dc7a3a70ebfe1ac4d1aaa3ad7e
Juan Esteban Zapata <juanes0202@gmail.com>
2020-11-17 10:15:51 -0500

    wip

--- resources/views/livewire/list-message.blade.php
+++ resources/views/livewire/list-message.blade.php
@@ -8,7 +8,7 @@
         <div class="media w-50 ml-auto mb-3">
             <div class="media-body">
                 <div class="bg-primary rounded py-2 px-3 mb-2" title="{{$message['from']}}">
-                    <p class="text-small mb-0 text-white">{{ $message['to'] }}</p>
+                    <p class="text-small mb-0 text-white">{{ $message['message'] }}</p>
                 </div>
                 <p class="small text-muted">{{ now()->format('h:m') }} | {{ now()->format('M d')}}</p>
             </div>
@@ -17,7 +17,7 @@
         <!-- Sender Message-->
         <div class="media w-50 mb-3"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" class="rounded-circle">
             <div class="media-body ml-3">
-                <div class="bg-light rounded py-2 px-3 mb-2" title="{{ $message['to'] }}">
+                <div class="bg-light rounded py-2 px-3 mb-2" title="{{ $message['from'] }}">
                     <p class="text-small mb-0 text-muted"> {{ $message['message'] }}</p>
                 </div>
                 <p class="small text-muted">{{ now()->format('h:m') }} | {{ now()->format('M d')}}</p>
